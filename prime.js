e6acd12c7efa22b6b42b369035a93a49a656fc6e
harshshukla2002 <harshshukla2312@gmail.com>
2022-09-13 15:30:50 +0530

    Prime Number

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num=15;
+let bag=true;
+for(i=2;i<num;i++){
+     if(num%i==0){
+          bag=false;            
+}
+}
+if(bag==false){
+   console.log("Prime Number");
+}
+else {
+   console.log("Not Prime"); 
+}
